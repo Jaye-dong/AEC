ebd6d07bfaacd26ce035bc0e20cbfd297462b5d2
Jaye Dong <1215859224@qq.com>
2022-09-14 11:35:36 +0800

    add audio

--- FDKF_test.m
+++ FDKF_test.m
@@ -1,27 +1,22 @@
+clc;
 clear;
-[x, fs_far] = audioread('../audio/8kHz_far.wav');
-[d, fs_near] = audioread('../audio/8kHz_near.wav');
-
-mu = 0.04;
-M = 4;
-mu_unconst = 0.04;
-select = 1;
-[en, yk, W] = FDAF(d,x,mu,mu_unconst, M, select);
-%sound(en,16000)
-%sound(d,16000)
-addpath 'D:\code\m\AEC\ERLE';
-erle = calc_ERLE(en,d,M);
-%plot(erle)
-%nanmean(erle)
-
-nanmean(erle)
-%maxerle = max(nanmean(erle),maxerle)
+[x, fs_far] = audioread('./audio/8kHz_far.wav');
+[d, fs_far_echo] = audioread('./audio/8kHz_near.wav');
+M = 32;beta=0.95;sgm2u=1e-2;sgm2v=1e-6;
+%% FDKF
+e = FDKF(x, d, M, beta, sgm2u, sgm2v);
+e = [e;d(length(e)+1:end)];
+% %% 写入txt
+% fid = fopen("D:\code\m\AEC\C_results\e_m.txt","w");% 写入文件路径	
+% for i=1:length(e)
+% fprintf(fid,'%.16f, ',e(i));		% 按行输出
+% end
+% fclose(fid);		% a为要保存的变量名，记得更改!!!
 %% 画图
 plot(d,"c");
 hold on;
-plot(en,"b");
+plot(e,"b");
 ylim([-1 1]);
-title("NLMS AEC")
+title("FDKF AEC")
 ylabel("amplitude");
 xlabel("samples")
-
